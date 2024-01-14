3d91154453ce9c72df5072fa4651dc4927b09df9
FernandoZuchi <nandozuchi07@gmail.com>
2024-01-14 20:54:11 -0300

    Adicionei um miniprojeto que contém um algorítmo para calcular sua idade

---
+++ Age-calculator/main.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+def calcular_idade(data_nascimento):
+    data_atual = datetime.now()
+    diferenca = data_atual - data_nascimento
+    idade = diferenca.days // 365
+    return idade
+
+def citeste_data_nascimento():
+    while True:
+        try:
+            data = input("Introduza a data de nascimento (YYYY-MM-DD): ")
+            data_nascimento = datetime.strptime(data, "%Y-%m-%d")
+            return data_nascimento
+        except ValueError:
+            print("A data inserida não é valida.")
+
+if __name__ == "__main__":
+    data_nascimento = citeste_data_nascimento()
+    idade = calcular_idade(data_nascimento)
+    print(f"Você tem {idade} anos")
