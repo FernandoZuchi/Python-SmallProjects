fe6d88ffa11be5fc5439164fd546fa0b4592d0d7
FernandoZuchi <nandozuchi07@gmail.com>
2024-01-21 15:54:03 -0300

    Adicionando um segundo botão

--- Button-Python/main.py
+++ Button-Python/main.py
@@ -3,10 +3,16 @@ import tkinter as tk
 def action_1():
     print("Botão 1 pressionado!")
 
+def action_2():
+    print("Botão 2 pressionado!")
+
 root = tk.Tk()
 root.title("GUI with Buttons")
 
-button1 = tk.Button(root, text="Button1", command=action_1)
+button1 = tk.Button(root, text="Botão 1", command=action_1)
 button1.pack()
+
+button2 = tk.Button(root, text="Botão 2", command=action_2)
+button2.pack()
 
 root.mainloop()
