7b287f4d4b999e44a2ea1ae027df1d683dbf406e
FernandoZuchi <nandozuchi07@gmail.com>
2024-01-21 15:55:18 -0300

    GUI com botões para fins de teste, biblioteca tkinter python

--- Button-Python/main.py
+++ Button-Python/main.py
@@ -6,13 +6,19 @@ def action_1():
 def action_2():
     print("Botão 2 pressionado!")
 
+def action_3():
+    print("Botão 3 pressionado!")
+
 root = tk.Tk()
-root.title("GUI with Buttons")
+root.title("GUI com botões")
 
 button1 = tk.Button(root, text="Botão 1", command=action_1)
 button1.pack()
 
 button2 = tk.Button(root, text="Botão 2", command=action_2)
 button2.pack()
+
+button3 = tk.Button(root, text="Botão 3", command=action_3)
+button3.pack()
 
 root.mainloop()
